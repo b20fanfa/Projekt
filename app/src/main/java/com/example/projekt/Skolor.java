2cc7cde397b20645fe1878d9445ceb666fbc2d07
Fanny Falck <b20fanfa@student.his.se>
2021-05-07 09:27:03 +0200

    Skapade en java class som heter "Skolor.java".

---
+++ app/src/main/java/com/example/projekt/Skolor.java
@@ -0,0 +1,13 @@
+package com.example.projekt;
+
+public class Skolor {
+
+    //Member variables
+    private String ID;
+    private String name;
+    private String type;
+    private String location;
+    private String category;
+
+    private int size;
+}
